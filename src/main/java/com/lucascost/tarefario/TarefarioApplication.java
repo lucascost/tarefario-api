d71d3245f7779a06a7e86fb848f282eda5e5b8e7
lucascost <lcyancosta@gmail.com>
2023-08-28 20:15:39 -0300

    Commit inicial

---
+++ src/main/java/com/lucascost/tarefario/TarefarioApplication.java
@@ -0,0 +1,13 @@
+package com.lucascost.tarefario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucascost/tarefario/TarefarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascost.tarefario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
